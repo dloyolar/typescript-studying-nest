dc70df5d6d058b8be20396e2d8ed75c0c72eb7bd
dloyolar <dieegolr@gmail.com>
2022-09-13 23:37:39 -0300

    add classes file

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,11 @@
+import { charmander } from './bases/03-classes';
 import './style.css';
-import typescriptLogo from './typescript.svg';
-import { setupCounter } from './counter';
-import { name, age } from './bases/01-types';
+// import { name, age } from './bases/01-types';
+
+console.log(charmander);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>${name} ${age}</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-
+    <h1>Hello ${charmander.name}</h1>
   </div>
 `;
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
