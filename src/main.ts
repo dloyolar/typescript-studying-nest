e175179f3040bb822c7e98ea85264c196fdaa04f
dloyolar <dieegolr@gmail.com>
2022-10-30 14:30:08 -0300

    add second part of decorators

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 // import { charmander } from './bases/03-classes';
 // import { name, age } from './bases/01-types';
 // import { charmander } from './bases/04-injection';
-import { charmander } from './bases/05-decorators';
+// import { charmander } from './bases/05-decorators';
+import { charmander } from './bases/06-decorators2';
+
 import './style.css';
 
 console.log(charmander);
